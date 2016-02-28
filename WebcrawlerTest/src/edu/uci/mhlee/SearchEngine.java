a2ec4ab74e327bdc5b1b90dbe6c03b8c87b86996
mhlee <mhlee1215@gmail.com>
2016-02-27 17:46:56 -0800

    Basic Level of search engine added.

--- WebcrawlerTest/src/edu/uci/mhlee/SearchEngine.java
+++ WebcrawlerTest/src/edu/uci/mhlee/SearchEngine.java
@@ -24,12 +24,12 @@ public class SearchEngine {
 		// TODO Auto-generated method stub
 
 		Map<Integer, Double> tfidfMap = new HashMap<Integer, Double>();
-		
-		
-		Scanner keyboard = new Scanner(System.in);
-		
-		String query = "INIT";
-		
+
+
+		//Scanner keyboard = new Scanner(System.in);
+
+		String query = "computer science";
+
 		Connection connection = null;
 		try
 		{
@@ -38,41 +38,120 @@ public class SearchEngine {
 			Statement statement = connection.createStatement();
 			statement.setQueryTimeout(30);  // set timeout to 30 sec.
 
-			
-			
-			while(query.length() >= 0){
-				query = keyboard.nextLine();
-				String[] queryParts = query.split(" ");
-				
-				String word;
-				int tf, df, docid;
-				double tfidf;
-				String pos;
-				for(String queryWord : queryParts){
-					
-					// First load DFs into dfMap
-					
-					String curQuery = "select word, docid, tf, df, tfidf, pos from invertedIndex where word = '"+queryWord+"'";
-					System.out.println(curQuery);
-					ResultSet rs = statement.executeQuery(curQuery);
 
-					while (rs.next())
-					{
-						// read the result set
-						word = rs.getString("word");
-						docid = rs.getInt("docid");
-						tf = rs.getInt("tf");
-						df = rs.getInt("df");
-						tfidf = rs.getDouble("tfidf");
-						pos = rs.getString("pos");
-						
-						
-					}
+
+			//while(query.length() >= 0){
+			//query = keyboard.nextLine();
+			String[] queryParts = query.split(" ");
+
+			String word;
+			int tf, df, docid;
+			double tfidf;
+			String pos;
+			for(String queryWord : queryParts){
+
+				// First load DFs into dfMap
+
+				String curQuery = "select word, docid, tf, df, tfidf, pos from invertedIndex where word = '"+queryWord+"'";
+				System.out.println(curQuery);
+				ResultSet rs = statement.executeQuery(curQuery);
+
+				while (rs.next())
+				{
+					// read the result set
+					word = rs.getString("word");
+					docid = rs.getInt("docid");
+					tf = rs.getInt("tf");
+					df = rs.getInt("df");
+					tfidf = rs.getDouble("tfidf");
+					pos = rs.getString("pos");
+
+					if(tfidfMap.get(docid) == null) tfidfMap.put(docid, 0.0);
+					tfidfMap.put(docid, tfidfMap.get(docid)+tfidf);
+
 				}
+			}
+			//}
+
+			tfidfMap = Utils.sortByValueDouble(tfidfMap);
+
+			System.out.println("--------------------------------------");
+		}
+		catch(SQLException e)
+		{
+			// if the error message is "out of memory", 
+			// it probably means no database file is found
+			System.err.println(e.getMessage());
+		}
+		finally
+		{
+			try
+			{
+				if(connection != null)
+					connection.close();
+			}
+			catch(SQLException e)
+			{
+				// connection close failed.
+				System.err.println(e);
+			}
+		}
+
+		print(tfidfMap);		
+
+
+	}
+
+
+	public static void print(Map<Integer, Double> wordFrequencies){
+
+		Connection connection = null;
+		try
+		{
+			// create a database connection
+			connection = DriverManager.getConnection("jdbc:sqlite:"+pr.getDbPath());
+			Statement statement = connection.createStatement();
+			statement.setQueryTimeout(30);  // set timeout to 30 sec.
+
+
+
+			//while(query.length() >= 0){
+			//query = keyboard.nextLine();
+
+			int docid;
+			String url;
+			String subdomain;
+			String path;
+			int j = 0;
+			for(Integer key : wordFrequencies.keySet()){
+				//System.out.println(key+"/"+wordFrequencies.get(key));
+				
+				String curQuery = "select docid, url, subdomain, path from webContents where docid = "+key;
+				//System.out.println(curQuery);
+				ResultSet rs = statement.executeQuery(curQuery);
+
+				while (rs.next())
+				{
+					// read the result set
+					docid = rs.getInt("docid");
+					url = rs.getString("url");
+					subdomain = rs.getString("subdomain");
+					path = rs.getString("path");
+					
+					System.out.println("Rank <"+(j+1)+"> Score:"+wordFrequencies.get(key)+", DocID: "+docid+" / URL: "+url+" / SubDomain: "+subdomain+" / Path:"+path);
+					System.out.println("");
+				}
+				
+				
+				
+				j++;
+				if(j > 10) break;
 			}
 			
 
-			System.out.println("--------------------------------------");
+			
+
+
 
 
 		}
@@ -95,14 +174,11 @@ public class SearchEngine {
 				System.err.println(e);
 			}
 		}
-		
-		
+
+
 	}
-	
-	
-	
-	
-	
-	
+
+
+
 
 }
